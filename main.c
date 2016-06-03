1395c42b28c6cc98b769cab32f1e2fc0ea8ccdd1
John <joaosisanoski@live.com>
2016-06-02 22:28:30 -0300

    Continuando implementacao de uma maquina deturing

--- main.c
+++ main.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-typedef struct {
-    char leitura;
-    char escrita;
-}acao;
+char alfabeto[30];
+
 typedef struct _estado{
-    acao *acao_fita;
+    char gravacao;
     char move_fita;
     int proximo_estado;
 
@@ -17,16 +15,25 @@ int get_index_from_char(char *string, char c){
     return (int)(e - string);
 
 }
+void testeExpressao(estado **maquina, char *string){
+    char *index = string;
+    int x = get_index_from_char(alfabeto, &(index));
+
+
+
+
+
+}
 int main(int argc , char *argv[])
 {
     FILE *file_input;
-    int **tabela_transicao;
-    char alfabeto[30];
+    estado **tabela_transicao;
     int numero_transicoes, numero_estados;
     int contagem_alfabeto;
-    int x;
-
-
+    int x, aux_int;
+    char aux;
+    int estado_atual;
+    char leitura_atual;
     if (argc < 1){
         exit(EXIT_FAILURE);
     }
@@ -37,21 +44,32 @@ int main(int argc , char *argv[])
     fscanf(file_input, "%s", alfabeto);
     printf("%s", alfabeto);
     fscanf(file_input, "%d", &numero_estados);
-    //fscanf(file_input, "%d", &numero_transicoes);
-
-
+    fscanf(file_input, "%d", &numero_transicoes);
     contagem_alfabeto = strlen(alfabeto) + 1;
-    tabela_transicao = (int **)malloc(contagem_alfabeto  * sizeof(int*));
+    tabela_transicao = (estado **)malloc(contagem_alfabeto  * sizeof(estado*));
     for( x= 0; x < contagem_alfabeto; x++)
-        tabela_transicao[ x ] = (int *)malloc(numero_estados * sizeof(int));
-
-
-
+        tabela_transicao[ x ] = (estado *)malloc(numero_estados * sizeof(estado));
+    //consome quebra de linha
+    fscanf(file_input, "%c", &aux);
     x = 0;
     while (x < numero_transicoes){
-
+        fscanf(file_input, "%d", &estado_atual);
+        estado_atual--;
+        fscanf(file_input, "%*[ \n\t]%c", &leitura_atual);
+        estado novo_estado;
+        fscanf(file_input, "%*[ \n\t]%c", &(novo_estado.gravacao));
+        fscanf(file_input, "%*[ \n\t]%c", &(novo_estado.move_fita));
+        fscanf(file_input, "%d", &(novo_estado.proximo_estado));
+        //consome quebra de linha
+        fscanf(file_input, "%c", &aux);
+        aux_int = get_index_from_char(alfabeto, leitura_atual);
+        if (leitura_atual == '-'){
+            aux_int = contagem_alfabeto-1;
+        }
+        tabela_transicao[aux_int][estado_atual] = novo_estado;
         x++;
     }
+    fscanf(file_input, "%d", &(numero_transicoes));
 
 
     return 0;
